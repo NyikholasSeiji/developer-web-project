610fc24705e4501c27bf6ca6762285bf68bcaa32
NyikholasSeiji <seijinyikholas@yahoo.com.br>
2026-08-19 14:36:43 -0300

    feat(app): habilita withComponentInputBinding no router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { REPOSITORY_PROVIDERS } from './core/config/repository.providers';
@@ -7,7 +7,7 @@ import { REPOSITORY_PROVIDERS } from './core/config/repository.providers';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     ...REPOSITORY_PROVIDERS,
   ]
 };
